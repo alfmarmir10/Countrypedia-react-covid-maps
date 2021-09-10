a46168e4b4130bd9fcdea46b35c9310081968804
alfmarmir10 <alfmar100391@icloud.com>
2021-09-10 16:01:54 -0500

    Fixed error useState of react.development

--- src/Components/Details/DetailsCard.jsx
+++ src/Components/Details/DetailsCard.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react/cjs/react.development';
+import React, { useEffect, useState } from 'react'
 import Map from './Map';
 import Chart from './Chart';
 
