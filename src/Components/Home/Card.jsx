d542ae71c82ab91180c3211f8e189a9ab856ea64
alfmarmir10 <alfmar100391@icloud.com>
2021-09-14 18:37:36 -0500

    Vercel Warnings fixed

--- src/Components/Home/Card.jsx
+++ src/Components/Home/Card.jsx
@@ -17,7 +17,7 @@ function Card(props) {
   }, [handleFetch])
 
   return (
-    <div ref={lastElementRef} className="card card-item" onScroll={handleScroll}>
+    <div ref={lastElementRef} className="card card-item">
       <img src={url} alt="Country img" className="card-img margin-top-sm"/>
       <p className="font-size-md font-weight-bold text-align-center margin-top-sm">{name}</p>
       {/* <button className="width-80percent border-radious-5px margin-top-sm bg-yellow font-size-md font-weight-bold btn-details" >Details</button> */}
